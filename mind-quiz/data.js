90a47f535879543bd4e2811d408520748514fe8c
hwecqi <66620947+hwecqi@users.noreply.github.com>
2021-01-14 10:18:08 +0800

    Update data.js

--- mind-quiz/data.js
+++ mind-quiz/data.js
@@ -1,8 +1,4 @@
-let images = [
-    "https://uc.udn.com.tw/photo/2019/12/11/realtime/7183211.jpg",
-    "https://d3l76hx23vw40a.cloudfront.net/recipe/yqn093-027c.jpg",
-    "https://foodpicks.tw/wp-content/uploads/2019-11-03_114250-696x406.jpg"
-mind-quiz/411942let questions = [
+let questions = [
     {
         "question":"你現在對今年非常樂觀？",
         "answers":[
